bb911d467898933a2d14ccad22d4b804eb33155b
Schmelzofen <Schmelzofen@users.noreply.github.com>
2023-05-24 09:30:40 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="Schmelzofen"
+original_name="robotfwt"
+original_urlname="robotfwt"
+original_description="Awesome robotfwt created by Schmelzofen"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv robotfwt $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
